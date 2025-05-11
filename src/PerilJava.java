7bc27cfd8e910dd47a1a022bf0f25eee4e3c991e
LaryssaGabi <lary.gabriela85@gmail.com>
2025-05-10 21:32:23 -0300

    Estudos iniciais

---
+++ src/PerilJava.java
@@ -0,0 +1,6 @@
+public class PerilJava {
+    public static void main(String[] args) {
+        System.out.println("Olá, Laryssa!");
+        System.out.print("Tudo bem com você?");
+    }
+}
---
+++ src/Subtracao.java
@@ -0,0 +1,6 @@
+public class Subtracao {
+    public static void main(String[] args){
+        System.out.println("Agora vamos realizar subtrações ;)");
+        System.out.println("10-5 = "+ (10-5));
+    }
+}
